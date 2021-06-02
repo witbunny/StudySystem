4f7fc0bb63dbae318e1245ad2ec15b2a2876c332
witbunny <witbunny@163.com>
2021-06-02 19:28:57 +0800

    sql: test table column (create/drop/alter) and table row (select/insert/update/delete)

--- staticpage/wwwroot/test/sql/SQLQueryTest.sql
+++ staticpage/wwwroot/test/sql/SQLQueryTest.sql
@@ -15,3 +15,66 @@ set single_user with rollback immediate --将数据库回滚到原始配置状�
 BACKUP DATABASE [SSDBtest] TO DISK = 'E:\echo\zero\adn\adnWork\StudySystem\staticpage\wwwroot\test\sql\SSDBtest.bak' 
 -- 根据备份恢复数据库
 RESTORE DATABASE [SSDBtest] FROM DISK = 'E:\echo\zero\adn\adnWork\StudySystem\staticpage\wwwroot\test\sql\SSDBtest.bak' 
+
+
+--create database test001;
+--go
+--use test001;
+
+--create table teacher
+--(
+--	id int 
+--);
+
+--use master;
+--drop database test001;
+
+create table student 
+(
+	id int
+);
+
+alter table student add score decimal(3,1);
+alter table student drop column score;
+alter table student alter column score float not null;
+
+drop table Teacher;
+
+--
+
+insert [Table] (Id,[name],age) values (3,N'zhuliang2',33);
+
+INSERT [Table]([Name], Age) VALUES(2,N'陈元',23);
+
+USE TEST001;
+ALTER TABLE student 
+--ADD [name] NVARCHAR NULL;
+--ALTER COLUMN [name] NVARCHAR(4000);
+--ALTER COLUMN score float null;
+ALTER COLUMN id int  null;
+
+INSERT student ([name],score) VALUES (N'ZL',56.7)
+
+SELECT * FROM student;
+
+SELECT score FROM student;
+
+BEGIN TRANSACTION
+--UPDATE student SET score = 30;
+--DELETE student;
+TRUNCATE TABLE Student;
+
+ROLLBACK;
+
+SELECT 1
+--WHERE 3 < 2
+WHERE EXISTS (
+	SELECT * FROM [Table]
+	WHERE ID = 2
+
+	SELECT * FROM [Table]
+	WHERE ID = 5
+)
+
+SELECT * FROM [Table]
+WHERE isfemale IS NULL
---
+++ staticpage/wwwroot/test/sql/Task-SQL-Base.sql
@@ -0,0 +1,68 @@
+﻿
+CREATE DATABASE TaskSQL;
+
+BACKUP DATABASE TaskSQL TO DISK = 'E:\echo\zero\adn\adnWork\StudySystem\staticpage\wwwroot\test\sql\TaskSQL.bak';
+
+DROP DATABASE TaskSQL;
+
+RESTORE DATABASE TaskSQL FROM DISK = 'E:\echo\zero\adn\adnWork\StudySystem\staticpage\wwwroot\test\sql\TaskSQL.bak';
+
+USE TaskSQL;
+
+CREATE TABLE [User] 
+(
+	ID INT NOT NULL,
+	UserName NVARCHAR(20) NOT NULL,
+	[Password] NVARCHAR(20) NOT NULL,
+	Describe NVARCHAR(40) NULL
+);
+
+CREATE TABLE Keyword 
+(
+	[Name] NVARCHAR(10) NOT NULL,
+	Used INT NULL
+);
+
+ALTER TABLE [User] 
+--ADD InvitedBy INT NULL;
+--ALTER COLUMN InvitedBy NVARCHAR(20);
+--DROP COLUMN InvitedBy;
+ALTER COLUMN [Password] NVARCHAR(20) NULL;
+
+CREATE TABLE Problem 
+(
+	Id INT NOT NULL,
+	Title NVARCHAR(20) NOT NULL,
+	NeedRemoteHelp BIT NULL,
+	Reward INT NULL,
+	PublishDateTime DATETIME NOT NULL
+);
+
+SELECT * FROM [User];
+
+INSERT [User] (ID,UserName,[Password]) VALUES (1,'17bang','1234');
+
+INSERT [User] (ID,UserName,[Password]) VALUES (2,'Admin',NULL);
+
+INSERT [User] (ID,UserName,[Password]) VALUES (3,'Admin-1','');
+
+INSERT [User] (ID,UserName,[Password]) VALUES (4,'SuperAdmin','123456');
+
+BEGIN TRANSACTION
+UPDATE [User] SET Password = 0;
+
+ROLLBACK;
+
+BEGIN TRANSACTION
+--DELETE [User];
+TRUNCATE TABLE [User];
+
+ROLLBACK;
+
+
+SELECT * FROM Keyword;
+
+BEGIN TRAN
+DROP TABLE Keyword;
+
+ROLLBACK;
