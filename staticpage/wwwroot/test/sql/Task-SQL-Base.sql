1bd63c43449622c00be54d6bd914cabf29acb7ba
witbunny <witbunny@163.com>
2021-06-04 02:04:24 +0800

    sql: test sql operator and where

--- staticpage/wwwroot/test/sql/Task-SQL-Base.sql
+++ staticpage/wwwroot/test/sql/Task-SQL-Base.sql
@@ -66,3 +66,62 @@ BEGIN TRAN
 DROP TABLE Keyword;
 
 ROLLBACK;
+
+SELECT * FROM [User] WHERE [Password] IS NULL OR [Password] = '';
+
+BEGIN TRAN
+DELETE [User] WHERE UserName LIKE '%Admin%' OR UserName LIKE '%17bang%';
+
+ROLLBACK;
+
+SELECT * FROM Problem;
+
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (1,N'如何搭建运行环境',1,10,'2019-9-10 09:10:20');
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (2,N'如何更改VS界面主题',0,22,'2019-9-11 10:10:20');
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (3,N'如何更改VS编辑区字体',0,20,'2019-10-11 10:10:20');
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (4,N'如何调试程序',1,25,'2019-11-11 11:10:20');
+
+BEGIN TRAN 
+UPDATE Problem SET Title = N'【推荐】' + Title WHERE Reward > 10;
+
+ROLLBACK;
+COMMIT;
+
+BEGIN TRAN 
+UPDATE Problem SET Title = N'【加急】' + Title WHERE Reward > 20 AND PublishDateTime > '2019-10-10 00:00:00';
+
+ROLLBACK;
+COMMIT;
+
+BEGIN TRAN 
+DELETE Problem WHERE Title LIKE N'【%】%';
+
+ROLLBACK;
+
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (5,N'【如何创建数据库',1,30,'2019-12-11 11:10:20');
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (6,N'如何创建数据库100%',1,35,'2019-12-12 11:10:20');
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (7,N'如何创建表100%',1,45,'2019-12-13 11:10:20');
+INSERT Problem (Id,Title,NeedRemoteHelp,Reward,PublishDateTime) VALUES (8,N'如何创建表',1,45,'2019-12-13 11:10:20');
+
+SELECT * FROM Problem WHERE Id >= 5 AND Title NOT LIKE N'%数据库%' AND Title LIKE '%#%%' ESCAPE '#';
+
+SELECT * FROM Keyword;
+
+INSERT Keyword ([Name],Used) VALUES (N'数据库',5);
+INSERT Keyword ([Name],Used) VALUES (N'关系型',10);
+INSERT Keyword ([Name],Used) VALUES (N'主键',15);
+INSERT Keyword ([Name],Used) VALUES (N'外键',20);
+INSERT Keyword ([Name],Used) VALUES (N'非空',0);
+INSERT Keyword ([Name],Used) VALUES (N'约束',-1);
+INSERT Keyword ([Name],Used) VALUES (N'唯一',NULL);
+INSERT Keyword ([Name],Used) VALUES (N'编程',105);
+
+SELECT [Name] FROM Keyword WHERE Used > 10 AND Used < 50;
+
+BEGIN TRAN 
+--UPDATE Keyword SET Used = 1 WHERE Used <= 0 OR Used IS NULL OR Used > 100;
+DELETE Keyword WHERE Used % 2 != 0;
+
+ROLLBACK;
+
+SELECT 1 WHERE NULL % 2 != 0;
