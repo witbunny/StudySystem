ac4efb16cbd7a8754b980c8301ece274474e1ed9
witbunny <witbunny@163.com>
2021-11-04 21:49:03 +0800

    C#: finished the homework of anonymous method / Lambda / closure

---
+++ cprocess/Base/Water.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cprocess
+{
+	
+	public delegate int ProvideWater(Person person);
+	public delegate int Water(ProvideWater pw);
+
+	public class Person
+	{
+		public int Age { get; set; }
+		public int Height { get; set; }
+		public int Weight { get; set; }
+	}
+}
---
+++ nutesttdd/WaterTests.cs
@@ -0,0 +1,68 @@
+﻿using cprocess;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nutesttdd
+{
+	class WaterTests
+	{
+		[Test]
+		public void WaterTest()
+		{
+			Person person = new Person()
+			{
+				Age = 23,
+				Height = 170,
+				Weight = 65
+			};
+			ProvideWater provide;
+
+			provide = AgeWater;
+			Assert.AreEqual(23, provide(person));
+
+			provide = delegate (Person person)
+			{
+				return person.Height;
+			};
+			Assert.AreEqual(170, provide(person));
+
+			provide = p =>  p.Weight;
+			Assert.AreEqual(65, provide(person));
+
+
+			Assert.AreEqual(23, GetWater(person, AgeWater));
+			Assert.AreEqual(170, GetWater(person, HeightWater));
+			Assert.AreEqual(65, GetWater(person, WeightWater));
+
+
+			Water water = delegate (ProvideWater pw)
+			{
+				return pw(person);
+			};
+			Assert.AreEqual(23, water(AgeWater));
+			Assert.AreEqual(170, water(HeightWater));
+			Assert.AreEqual(65, water(WeightWater));
+
+		}
+
+		public static int GetWater(Person person, ProvideWater pw)
+		{
+			return pw(person);
+		}
+
+		public static int AgeWater(Person person)
+		{
+			return person.Age;
+		}
+		public static int HeightWater(Person person)
+		{
+			return person.Height;
+		}
+		public static int WeightWater(Person person)
+		{
+			return person.Weight;
+		}
+	}
+}
