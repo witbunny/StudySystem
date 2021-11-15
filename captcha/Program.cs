01ff0c4ad76fb6431b9165b962f5fb7d81c394f6
witbunny <witbunny@163.com>
2021-11-15 15:07:30 +0800

    C#: study IO / drawing

---
+++ captcha/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace captcha
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Bitmap image = new Bitmap(500, 200);
+			Graphics g = Graphics.FromImage(image);
+			g.Clear(Color.AliceBlue);
+
+			g.DrawLine(new Pen(Color.Black), new Point(0, 0), new Point(300, 150));
+			g.DrawString("qwrt", 
+				new Font("宋体", 12), 
+				new SolidBrush(Color.DarkGray), 
+				new PointF(20, 30));
+
+			image.SetPixel(100, 100, Color.Orange);
+
+			image.Save(@"E:\echo\zero\adn\test\testing\hello.jpg", ImageFormat.Jpeg);
+
+		}
+	}
+}
--- cprocess/Program.cs
+++ cprocess/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -64,7 +66,64 @@ namespace cprocess
 			//Console.WriteLine(task.Result);
 			//Process();
 
-			
+			//IO
+			//path
+			string folderpath = @"E:\echo\zero\adn";
+			//string fullpath = Path.Join(folderpath, @"test.text");
+			string fullpath = Path.Combine(folderpath, @"test.text");
+			Console.WriteLine(fullpath);
+			Console.WriteLine("HasExtension:" + Path.HasExtension(fullpath));
+			Console.WriteLine("GetExtension:" + Path.GetExtension(fullpath));
+			Console.WriteLine("GetFileNameWithoutExtension:" + Path.GetFileNameWithoutExtension(fullpath));
+			Console.WriteLine("GetDirectoryName:" + Path.GetDirectoryName(fullpath));
+
+			//directory
+			folderpath = Path.Combine(folderpath, @"test\testing");
+			Console.WriteLine(folderpath);
+			//Directory.CreateDirectory(folderpath);
+			//Directory.Delete(folderpath, false);
+			Console.WriteLine(Environment.CurrentDirectory);
+
+			//file
+			fullpath = Path.Combine(folderpath, @"test.text");
+			//FileStream stream = File.Create(fullpath);
+			//stream.Write(
+			//	new byte[4] { 33, 34, 35, 36 }, //要写入的字节
+			//	0,
+			//	4  /*缓冲的大小*/);
+			//stream.Flush();
+
+			FileStream stream = File.OpenRead(fullpath);
+			byte[] container = new byte[100];
+			stream.Read(container, 0, 100);
+			stream.Flush();
+			Console.WriteLine(Encoding.UTF8.GetString(container));
+
+			stream.Dispose();
+
+			/*
+			StreamWriter writer = File.AppendText(Path.Join(folderpath, "greet.txt"));
+			string greet = "吃饭了";
+			writer.Write(greet);
+			writer.WriteLine(true);
+			writer.Write(new char[] { 'l', 'c', 'b', 'c' });
+
+			//write() 之后还需要flush()才能真正的写入文件
+			writer.Flush();
+
+			//调用Dispose()释放文件资源，让其他“人”也可以操作该文件 
+			writer.Dispose();
+			*/
+
+			using (StreamWriter writer    //StreamWriter实现了IDisposable
+				= File.CreateText(Path.Join(folderpath, "greet.txt")))
+			{
+				writer.Write("吃饭了");
+			}//在using块结束时由writer调用其Dispose()方法
+
+
+
+
 
 			Console.Read();
 		}
