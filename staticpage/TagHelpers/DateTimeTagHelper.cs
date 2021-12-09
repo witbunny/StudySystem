9f593c9e626bb6acfbde7d01bcfd3a06cd2e37aa
witbunny <witbunny@163.com>
2021-12-10 03:16:37 +0800

    RP: finished the homework of tag helper custom

---
+++ staticpage/TagHelpers/DateTimeTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace staticpage.TagHelpers
+{
+	[HtmlTargetElement("datetime", Attributes = "asp-showicon, asp-only")]
+	public class DateTimeTagHelper : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "small";
+
+			string ctt = output.GetChildContentAsync().Result.GetContent();
+			DateTime dateTime = Convert.ToDateTime(ctt);
+
+			output.Content.SetContent(dateTime.ToString("yyyy年MM月dd日 HH时mm分ss秒"));
+
+			bool isshowicon = Convert.ToBoolean(context.AllAttributes["asp-showicon"].Value.ToString());
+
+			if (isshowicon)
+			{
+				output.PreContent.SetHtmlContent("<span class=\"fa fa-calendar\"></span>");
+			}
+
+			output.Attributes.RemoveAll("asp-showicon");
+
+			string only = context.AllAttributes["asp-only"].Value.ToString();
+
+			if (only == "date")
+			{
+				output.Content.SetContent(dateTime.ToString("yyyy年MM月dd日"));
+			}
+
+			output.Attributes.Remove(context.AllAttributes["asp-only"]);
+
+			base.Process(context, output);
+		}
+	}
+}
---
+++ staticpage/TagHelpers/PagerTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace staticpage.TagHelpers
+{
+	[HtmlTargetElement("pager", Attributes = "path, pageIndex")]
+	public class PagerTagHelper : TagHelper
+	{
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+			output.TagName = "a";
+
+			var path = context.AllAttributes["path"].Value;
+			var pageIndex = context.AllAttributes["pageIndex"].Value;
+
+			output.Attributes.Add("href", $"{path}/Page-{pageIndex}");
+
+			output.Attributes.Remove(context.AllAttributes["path"]);
+			output.Attributes.RemoveAll("pageIndex");
+
+			base.Process(context, output);
+		}
+	}
+}
