5a14b3baebf0c9b62851798f3ded597ec3fc1dcd
witbunny <witbunny@163.com>
2022-01-18 03:57:07 +0800

    ADO: study connection / command / execute and parameter / stored proedure / transaction

--- cprocess/Program.cs
+++ cprocess/Program.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace cprocess
 {
@@ -136,6 +140,7 @@ namespace cprocess
 
 			*/
 
+			/*
 			//现有一个txt文件，里面存放了若干email地址，使用分号（;）或者换行进行了分隔。请删除其中重复的email地址，并按每30个email一行（行内用;分隔）重新组织
 			string path = @"E:\echo\zero\adn\test\testing\email.txt";
 			string content = File.ReadAllText(path);
@@ -161,8 +166,211 @@ namespace cprocess
 			}
 			string newcontent = string.Join("\r\n", restrs);
 			File.WriteAllText(@"E:\echo\zero\adn\test\testing\reemail.txt", newcontent);
+			*/
 
-			
+
+			//string name = Console.ReadLine();
+			//string password = Console.ReadLine();
+
+			///1.连接数据库
+			///2.生成数据库命令
+			///3.执行
+			///4.关闭数据库
+
+			string connectionString1 = 
+				@"Data Source=localhost\SQLEXPRESS;
+				Initial Catalog=SSDBtest;
+				User ID=sa;
+				Password=0117;
+				Connect Timeout=30;
+				Encrypt=False;
+				TrustServerCertificate=False;
+				ApplicationIntent=ReadWrite;
+				MultiSubnetFailover=False";
+			string connectionString2 =
+				@"Data Source=localhost\SQLEXPRESS;
+				Initial Catalog=TaskSQL;
+				User ID=sa;
+				Password=0117;
+				Connect Timeout=30;
+				Encrypt=False;
+				TrustServerCertificate=False;
+				ApplicationIntent=ReadWrite;
+				MultiSubnetFailover=False";
+
+			//IDbConnection connection1 = new SqlConnection(connectionString1);
+			//IDbConnection connection2 = new SqlConnection(connectionString2);
+			//try
+			//{
+			//	connection1.Open();
+
+			//	IDbCommand command1 = new SqlCommand();
+			//	command1.Connection = connection1;
+			//	command1.CommandText = @"UPDATE StudentScore SET Score = 91 
+			//	WHERE Sname = 'ZL' AND Major = 'JAVA'";
+
+			//	Console.WriteLine(command1.ExecuteNonQuery());
+			//}
+			//catch (Exception)
+			//{
+
+			//	throw;
+			//}
+			//finally
+			//{
+			//	connection1.Close();
+			//	connection1.Dispose();
+			//	connection1 = null;
+			//}
+
+			/*
+			using (IDbConnection connection1 = new SqlConnection(connectionString1))
+			{
+				connection1.Open();
+
+				IDbCommand command1 = new SqlCommand();
+				command1.Connection = connection1;
+				//	command1.CommandText = @"UPDATE StudentScore SET Score = 91 
+				//WHERE Sname = 'ZL' AND Major = 'JAVA'";
+				command1.CommandText = @"SELECT * FROM StudentScore";
+				//command1.CommandText = @"SELECT Score FROM StudentScore WHERE Id > 10";
+				//command1.CommandText = @"SELECT Score FROM StudentScore WHERE Id = 9";
+
+				//Console.WriteLine(command1.ExecuteNonQuery());
+				//object count = command1.ExecuteScalar();
+
+				//IDataReader reader = command1.ExecuteReader();
+				DbDataReader reader = (DbDataReader)command1.ExecuteReader();
+				if (reader.HasRows)
+				{
+					while (reader.Read())
+					{
+						for (int i = 0; i < reader.FieldCount; i++)
+						{
+							Console.Write(reader[i] + "\t");
+						}
+						Console.WriteLine();
+					}
+				}
+			}
+			*/
+
+			/*
+			using (IDbConnection connection2 = new SqlConnection(connectionString2))
+			{
+				connection2.Open();
+
+				IDbCommand command2 = new SqlCommand();
+				command2.Connection = connection2;
+				//command2.CommandText = 
+				//	$"SELECT [Password] FROM [User] " +
+				//	$"WHERE UserName = '{name}'";
+
+				command2.CommandText =
+					$"SELECT [Password] FROM [User] " +
+					$"WHERE UserName = @Name";
+				IDataParameter pName = new SqlParameter("@Name", name);
+				command2.Parameters.Add(pName);
+
+
+				//object oPassword = command2.ExecuteScalar();
+				//if (oPassword == null)
+				//{
+				//	Console.WriteLine("用户名不存在");
+				//}
+				//else if (password != oPassword.ToString())
+				//{
+				//	Console.WriteLine("用户名或密码错误");
+				//}
+				//else
+				//{
+				//	Console.WriteLine("登录成功");
+				//}
+
+				string sPassword = command2.ExecuteScalar()?.ToString();
+				if (sPassword == null)
+				{
+					Console.WriteLine("用户名不存在");
+				}
+				else if (password != sPassword)
+				{
+					Console.WriteLine("用户名或密码错误");
+				}
+				else
+				{
+					Console.WriteLine("登录成功");
+				}
+			}
+			*/
+
+			using (IDbConnection connection2 = new SqlConnection(connectionString2))
+			{
+				connection2.Open();
+
+				IDbCommand command2 = new SqlCommand();
+				command2.CommandType = CommandType.StoredProcedure;
+
+				command2.Connection = connection2;
+				command2.CommandText = "UserRegister";
+
+				IDataParameter puna = new SqlParameter("@una", "toto");
+				IDataParameter pupw = new SqlParameter("@upw", "toto");
+				IDataParameter pivn = new SqlParameter("@ivn", "jojo");
+				IDataParameter pivc = new SqlParameter("@ivc", 6606);
+
+				IDataParameter poutcode = new SqlParameter("@outcode", SqlDbType.Int)
+				{
+					Direction = ParameterDirection.Output
+				};
+
+				command2.Parameters.Add(puna);
+				command2.Parameters.Add(pupw);
+				command2.Parameters.Add(pivn);
+				command2.Parameters.Add(pivc);
+				command2.Parameters.Add(poutcode);
+
+				Console.WriteLine(command2.ExecuteNonQuery());
+				Console.WriteLine(poutcode.Value);
+
+
+
+				//IDbCommand command = new SqlCommand();
+				//command.Connection = connection2;
+				//command.CommandText = 
+				//	"INSERT [Message] (FromUser, ToUser, Content) VALUES ('1', '2', '3');" +
+				//	"SELECT SCOPE_IDENTITY()";
+				//Console.WriteLine(command.ExecuteScalar());
+
+				//using (TransactionScope scope = new TransactionScope())
+				//{
+				//	//添加多个连接
+				//	scope.Complete();//提交
+				//	//scope.Dispose();//回滚释放
+				//}
+
+				using (IDbTransaction transaction = connection2.BeginTransaction())
+				{
+					try
+					{
+						IDbCommand command = new SqlCommand();
+						command.Connection = connection2;
+						command.Transaction = transaction;
+
+						command.CommandText = "UPDATE Credit SET Points += 1";
+						command.ExecuteNonQuery();
+
+						command.CommandText = "UPDATE Credit SET Points += 1";
+						command.ExecuteNonQuery();
+
+						transaction.Commit();
+					}
+					catch (Exception)
+					{
+						transaction.Rollback();
+						throw;
+					}
+				}
+			}
 
 
 			Console.Read();
---
+++ cprocess/SQLQueryCmd.sql
@@ -0,0 +1,24 @@
+﻿UPDATE StudentScore SET Score = 92 
+WHERE Sname = 'ZL' AND Major = 'JAVA'
+
+SELECT Score FROM StudentScore WHERE Id > 10
+
+SELECT Score FROM StudentScore WHERE Id = 9
+
+SELECT * FROM StudentScore
+
+--------------------------------------------------
+
+SELECT * FROM [User] WHERE UserName = 'Scorpio' AND [Password] = 'aa123456'
+
+SELECT [Password] FROM [User] WHERE UserName = 'Scor' OR 1=1 --'
+
+INSERT [Message] (FromUser, ToUser, Content) VALUES ('1', '2', '3');
+--SELECT @@IDENTITY
+SELECT SCOPE_IDENTITY()
+
+SELECT * FROM [Message]
+
+SELECT * FROM Credit
+
+UPDATE Credit SET Points += 1
