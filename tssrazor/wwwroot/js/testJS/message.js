c128f95300543e61bf4f03867d87a2adfcb8df2c
witbunny <witbunny@163.com>
2022-03-03 20:16:32 +0800

    JS: finished the homework of select all in MessageMine

---
+++ tssrazor/wwwroot/js/testJS/message.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    var oUp = document.getElementById("select-all-up");
+    var oDown = document.getElementById("select-all-down");
+
+    var aCheckbox = [];
+    var aInput = document.getElementsByTagName("input");
+    for (var i = 0; i < aInput.length; i++) {
+        if (aInput[i].type === "checkbox" && aInput[i].name.startsWith("Messages")) {
+            aCheckbox.push(aInput[i]);
+        }
+    }
+
+    oUp.onclick = selectAll;
+    oDown.onclick = selectAll;
+    function selectAll() {
+        var isChecked = this.checked;
+        for (var i = 0; i < aCheckbox.length; i++) {
+            aCheckbox[i].checked = isChecked;
+        }
+        oUp.checked = isChecked;
+        oDown.checked = isChecked;
+    }
+})();
