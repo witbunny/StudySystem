22fa43c37ce63406f3b147f3676541d69feb5d9f
witbunny <witbunny@163.com>
2022-03-03 17:56:16 +0800

    JS: finished the homework of event

---
+++ staticpage/wwwroot/js/events.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var aBtn = document.getElementsByTagName("button");
+    aBtn[0].onclick = function () {
+        history.back();
+    };
+    aBtn[1].onclick = function () {
+        history.forward();
+    };
+})();
+
+(function () {
+    var oA = document.getElementsByTagName("a")[0];
+    var oInput = document.getElementsByTagName("input")[0];
+    var oBtn = document.getElementById("go");
+    oA.onclick = function (event) {
+        event.preventDefault();
+        oInput.value = oA.href;
+    }
+    oBtn.onclick = function () {
+        open(oInput.value);
+    }
+})();
