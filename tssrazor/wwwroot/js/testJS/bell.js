904b44aad07e18927feade2e0846c9cadf116589
witbunny <witbunny@163.com>
2022-03-03 17:15:38 +0800

    JS: finished the homework of bell twinkle

---
+++ tssrazor/wwwroot/js/testJS/bell.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var oBell = document.getElementById("bell");
+    var iId = setInterval(function () {
+        if (oBell.classList.contains("lightblue")) {
+            oBell.classList.replace("lightblue", "darkblue");
+        }
+        else {
+            oBell.classList.replace("darkblue", "lightblue");
+        }
+    }, 1000);
+}) ();
