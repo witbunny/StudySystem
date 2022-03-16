f05dd04638f1026325424d500ebaea4a9f3f0030
witbunny <witbunny@163.com>
2022-03-16 14:48:10 +0800

    ES: finished the homework of ES

---
+++ staticpage/wwwroot/js/score.js
@@ -0,0 +1,66 @@
+﻿export { Score, bestOfAll as record }
+
+import { getName, login } from "./user.js";
+
+class Score {
+    constructor(name, score) {
+        this.datetime = new Date();
+        this.playName = name;
+        this.score = score;
+    }
+
+    static getBest(name) {
+        var best;
+        Score.allInstances.get(name).forEach(function (value) {
+            if (best == undefined) {
+                best = value;
+            } //else continue
+            if (value.score > best.score) {
+                best = value
+            } //else nothing
+        });
+        return best;
+    }
+
+    save() {
+        var hasScore = Score.allInstances.get(this.playName);
+        if (hasScore == undefined) {
+            Score.allInstances.set(this.playName, []);
+        } //else continue
+        Score.allInstances.get(this.playName).push(this);
+    }
+}
+
+Score.allInstances = new Map();
+
+let bestOfAll = function () {
+    var bestScore;
+    var userScore;
+    Score.allInstances.forEach(function (value, key) {
+        userScore = Score.getBest(key).score;
+        if (bestScore == undefined) {
+            bestScore = userScore;
+        } //else continue
+        if (userScore > bestScore) {
+            bestScore = userScore
+        } //else nothing
+    });
+    return bestScore;
+}
+
+var leo1 = new Score('leo', 85);
+var leo2 = new Score('leo', 95);
+var leo3 = new Score('leo', 75);
+leo1.save();
+leo2.save();
+leo3.save();
+
+var ana1 = new Score('ana', 69);
+var ana2 = new Score('ana', 99);
+ana1.save();
+ana2.save();
+
+//console.log(getName(2));
+//console.log(login('leo', '1234'));
+
+
---
+++ staticpage/wwwroot/js/user.js
@@ -0,0 +1,57 @@
+﻿export { getName, login }
+
+import { Score, record } from './score.js';
+
+class User {
+    constructor(id, name, password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        User.allInstances.push(this);
+    }
+
+    static getNameById(id) {
+        var name;
+        User.allInstances.forEach(function (value) {
+            if (value.id == id) {
+                name = value.name;
+            }
+        });
+        return name;
+    }
+
+    static login(name, password) {
+        var id;
+        User.allInstances.forEach(function (value) {
+            if (value.name === name && value.password === password) {
+                id = value.id;
+            }
+        });
+        return id;
+    }
+}
+
+User.allInstances = [];
+
+var leo = new User(1, 'leo', '1234');
+var ana = new User(2, 'ana', '1234');
+
+var getName = User.getNameById;
+var login = User.login;
+
+
+
+//var leo1 = new Score(leo.name, 85);
+//var leo2 = new Score(leo.name, 95);
+//var leo3 = new Score(leo.name, 75);
+//leo1.save();
+//leo2.save();
+//leo3.save();
+
+//var ana1 = new Score(ana.name, 69);
+//var ana2 = new Score(ana.name, 99);
+//ana1.save();
+//ana2.save();
+
+//console.log(record());
+
